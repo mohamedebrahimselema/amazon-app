34077be0e9a2202ceb73ce77b45247155d12af65
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-29 16:56:45 +0200

    create footer and header components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Pro Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="./Cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="./login">
+                <i className="fas fa-user"></i>
+                Sing in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
