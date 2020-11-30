f838ea53d7767319f1f1b22f2017c60d61e30536
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-30 09:19:57 +0200

    adding product details page

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,11 +7,11 @@ import "./Prodcut.css";
 const Product = (props) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Link to={`/products/${props.product._id}`}>
+      <Link to={`/product/${props.product._id}`}>
         <Card.Img src={props.product.image} variant="top"></Card.Img>
       </Link>
       <Card.Body>
-        <Link to={`/products/${props.product._id}`}>
+        <Link to={`/product/${props.product._id}`}>
           <Card.Title as="div">
             <strong>{props.product.name}</strong>
           </Card.Title>
