c249ee06de8872a464422613f69803ee310dc3a5
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-30 07:58:14 +0200

    adding home screen with products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Home from "./screen/Home";
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Learn React</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
