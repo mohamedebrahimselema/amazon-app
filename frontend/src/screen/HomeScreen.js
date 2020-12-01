52710a81368fddf01d68e69db6497a2eba43727a
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-12-01 23:02:33 +0200

    adding message component to show error

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -3,6 +3,8 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useDispatch, useSelector } from "react-redux";
 import listProducts from "../actions/productActions.js";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 import axios from "axios";
 const Home = () => {
   // const [products, setProducts] = useState([]);
@@ -26,9 +28,9 @@ const Home = () => {
     <>
       <h1>Latest Products </h1>
       {loading ? (
-        <h2>loading ... </h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
