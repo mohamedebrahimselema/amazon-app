20519baa65a9f967dfedd41ab9de628fd6a44932
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-12-01 12:24:12 +0200

    make backend and front end run concurentely

--- backend/service.js
+++ backend/service.js
@@ -1,16 +1,25 @@
-const express = require("express");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
 
-const products = require("./data/products");
-
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("bla bla bla ");
 });
 
+app.get("/api/products/", (req, res) => {
+  res.json(products);
+});
+
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log("server running on port 5000"));
+app.listen(
+  PORT,
+  console.log(`server running on ${process.env.NODE_ENV} port ${PORT}`)
+);
