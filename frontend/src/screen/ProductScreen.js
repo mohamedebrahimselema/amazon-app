60c552716fe1c92feee6c7dccb65b84ae10507fb
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-12-02 01:31:59 +0200

    adding add to cart btn function

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -18,7 +18,7 @@ import Loader from "../components/Loader";
 import { listProductDetails, listProducts } from "../actions/productActions";
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   // const [product, setProduct] = useState([]);
 
